a54151ee21e88f0ef2cdc89552af9dd846649990
adrianduncan <46533389+adrianduncan@users.noreply.github.com>
2020-08-02 10:27:39 -0700

    Added jQuery for Sticky Navigation Menu
    Added jQuery to create a sticky navigation menu.  Also, styled navigation and resized logo to fit design.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Sticky Navigation
+$(document).ready(function() {
+
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+           $('nav').addClass('sticky');
+        } else {
+           $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '65px;'
+      })
+
+
+});
